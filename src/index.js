01d8c54e7bb01b46b395e17caf478a65b9780b7e
Francisco Garcia <francisco.garcia.pilar@gmail.com>
2018-06-15 17:51:59 -0500

    chapter1-setting-up-mongodb

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const { graphql } = require('graphql');
 const readline = require('readline');
 const mySchema = require('../schema/main');
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
 
+// The readline interface code
 const rli = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rli.question('Client Request: ', inputQuery => {
-    graphql(mySchema, inputQuery).then(result => {
-        console.log('Server answer :', result.data);
-    });
+const MONGO_URL = 'mongodb://localhost:27017';
 
-    rli.close();
+MongoClient.connect(MONGO_URL, (err, client) => {
+    assert.equal(null, err);
+    console.log('Connected to MongoDB server');
+    const db = client.db('test');
+    rli.question('Client Request: ', inputQuery => {
+        graphql(mySchema, inputQuery, { }, { db }).then(result => {
+            console.log('Server answer :', result.data);
+            client.close(() => rli.close());
+        });
+    });
 });
